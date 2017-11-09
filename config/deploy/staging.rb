85d403d6df50a138ba6238088762921357b0213b
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-11-09 16:26:24 +0100

    fixed capistrano settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,4 +5,3 @@ role :db,  %w(kup.aspgems.com)
 set :app_env, 'master'          # Name used in rollbar/hipchat, etc
 set :branch,   fetch(:app_env)  # Git branch deployed
 set :instance, fetch(:branch)   # Deploy extra files from script/deploy/<instance>
-set :bundle_without, %w{development}.join(' ')  
